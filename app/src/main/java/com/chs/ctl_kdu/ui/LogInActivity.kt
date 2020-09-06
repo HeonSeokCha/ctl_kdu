581d0760d86b5c259113bcb061751dea7e79587b
Akalee <wooogi123@gmail.com>
2020-09-06 23:51:50 +0900

    All kotlin code refactoring

--- app/src/main/java/com/chs/ctl_kdu/ui/LogInActivity.kt
+++ app/src/main/java/com/chs/ctl_kdu/ui/LogInActivity.kt
@@ -4,41 +4,40 @@ import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.chs.ctl_kdu.R
 import com.chs.ctl_kdu.viewmodel.LogInViewModel
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var viewmodel: LogInViewModel
 
-    private lateinit var jsessionId:String
-    private lateinit var viewmodel:LogInViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         viewmodel = ViewModelProvider.AndroidViewModelFactory.getInstance(Application())
             .create(LogInViewModel::class.java)
+
         btn_do_login.setOnClickListener {
             if(input_userId.text!!.trim().isNotEmpty() && input_userPw.text!!.trim().isNotEmpty()){
-                doLogIn(input_userId.text!!.trim().toString(),input_userPw.text!!.trim().toString())
+                doLogin(input_userId.text!!.trim().toString(),input_userPw.text!!.trim().toString())
             } else{
                 Toast.makeText(this, "아이디와 비밀번호를 입력해주세요!!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    fun doLogIn(userId:String,userPw:String){
-        viewmodel.Login(userId,userPw).observe(this, Observer{
-            jsessionId = it
-            val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("jsessionId",jsessionId)
-            startActivity(intent)
-            finish()
+    private fun doLogin(userId: String, userPw: String) {
+        viewmodel.Login(userId, userPw).observe(this, Observer {
+            if (it) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Login Failure!", Toast.LENGTH_SHORT).show()
+            }
         })
     }
 }
--- app/src/main/java/com/chs/ctl_kdu/viewmodel/LogInViewModel.kt
+++ app/src/main/java/com/chs/ctl_kdu/viewmodel/LogInViewModel.kt
@@ -5,34 +5,26 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.chs.ctl_kdu.retrofitServices.retrofitApi
+import com.chs.ctl_kdu.retrofitServices.CtlApi
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class LogInViewModel:ViewModel(){
-    val retrofit = Retrofit.Builder()
-        .baseUrl("https://ctl.kduniv.ac.kr/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .build()
-    val service = retrofit.create(retrofitApi::class.java)
+    private val api = CtlApi.create()
 
-    fun Login(userId:String,userPw:String):LiveData<String>{
-        var mutableData = MutableLiveData<String>()
-        service.doLogin(userId,userPw,"UN").enqueue(object :Callback<User>{
+    fun Login(userId: String, userPw: String): LiveData<Boolean> {
+        val ret = MutableLiveData<Boolean>()
+        api.doLogin(userId, userPw, "UN").enqueue(object: Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
-                mutableData.value = response.headers()["Set-Cookie"]!!.split(";")[0]
+                ret.value = true
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
-                Log.d("Failed",t.localizedMessage)
-                mutableData.value = ""
+                Log.d("Failed", t.localizedMessage)
+                ret.value = false
             }
         })
-        return mutableData
+        return ret
     }
 }
